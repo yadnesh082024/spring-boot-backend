aa1954aba79a5f9d353045ec7905e6af10d21f8b
yadnesh <yadnesh082024@gmail.com>
2024-09-10 19:00:39 +0530

    fixing workflow

--- src/test/java/com/example/SimpleRestApiApplicationTests.java
+++ src/test/java/com/example/SimpleRestApiApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class SimpleRestApiApplicationTests {
 
+	@Autowired
+	private ApplicationContext applicationContext;
+
 	@Test
 	void contextLoads() {
+		assertNotNull(applicationContext, "The application context should have loaded.");
 	}
 
 }
